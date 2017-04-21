73720dee46ca913d945673337d5db812ef31e3eb
Subrata Sarker <subrata_sarker@yahoo.com>
2017-04-21 09:25:49 +0000

    wget fails to download json file due to 429 error

--- bin/new_reddits.py
+++ bin/new_reddits.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+import sys
+import time
 import wget
 import json
 
 
-def get_reddits(url, output = "./new_reddits.json"):
-    return wget.download(url, out = output)
+def get_reddits(url, output = "new-reddits.json"):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    return wget.download(url, "-".join([timestr,output]))
 
 def read_reddits(jsonfile):
     with open(jsonfile, "r") as f:
         data = json.loads(f.read())
 
-    reddits = data["data"]["children"]
-    for reddit in reddits:
-        edata = reddit["data"]
-        print "title: %s" % (edata["title"])
-        print "subreddit: %s" % (edata["subreddit"])
-        print "url: %s" % (edata["url"])
-        print "permalink: %s" % (edata["permalink"])
-        print "selftext_html: %s" % (edata["selftext_html"])
+    if "data" not in data.keys():
+        for key, value in data.items():
+            print ""
+            print "{}: {}".format(key, value)
+        return
+    
+    data = loaded_data["data"]
+    if "children" not in data.keys():
+        for key, value in data.items():
+            print ""
+            print "{}: {}".format(key, value)
+        return 
+    
+    reddits  = data["children"]
+    
+    if reddits:
+        for reddit in reddits:
+            edata = reddit["data"]
+            print "*******************************************"
+            print "*******************************************"
+            print "title: %s" % edata["title"]
+            print "subreddit: %s" % edata["subreddit"]
+            print "url: %s" % edata["url"]
+            print "permalink: %s" % edata["permalink"]
+            print "selftext_html: %s" % edata["selftext_html"]
+            print "*******************************************"
+            print "*******************************************"
+    else:
+        print "Something went wrong!"
 
 
 if __name__ == "__main__":
