6cd9d7035bc211630a92905727b6a25fed6202ea
Subrata Sarker <subrata_sarker@yahoo.com>
2018-08-15 06:31:27 +0000

    Update new_reddits.py to make it work with Python3

--- new_reddits.py
+++ new_reddits.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
 import sys
 import time
@@ -11,7 +11,7 @@ def get_reddits(url, output = "new-reddits.json"):
     returns full path of the downloaded file
     """
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    headers = {'User-agent': 'mincklu-bot: {}'.format(timestr)}
+    headers = {"User-agent": f"picklu-bot-0.3-{timestr}"}
     
     # get data from url
     r = requests.get(url, headers = headers)
@@ -28,16 +28,16 @@ def read_reddits(jsonfile):
         sys.exit("Failed load reddits!")
 
     if "data" not in data.keys():
-        print ""
+        print()
         for key, value in data.items():
-            print "{}: {}".format(key, value)
+            print(f"{key}: {value}")
         return
     
     data = data["data"]
     if "children" not in data.keys():
-        print ""
+        print()
         for key, value in data.items():
-            print "{}: {}".format(key, value)
+            print(f"{key}: {value}")
         return 
     
     reddits  = data["children"]
@@ -45,23 +45,21 @@ def read_reddits(jsonfile):
     if reddits:
         for reddit in reddits:
             if "data" not in reddit.keys():
-                print ""
+                print()
                 for key, value in reddit.items():
-                    print "{}: {}".format(key, value)
+                    print(f"{key}: {value}")
                 return
         
             edata = reddit["data"]
-            print "*******************************************"
-            print "*******************************************"
-            print "title: %s" % edata["title"]
-            print "subreddit: %s" % edata["subreddit"]
-            print "url: %s" % edata["url"]
-            print "permalink: %s" % edata["permalink"]
-            print "selftext_html: %s" % edata["selftext_html"]
-            print "*******************************************"
-            print "*******************************************"
+            print("*******************************************")
+            print(f"title: {edata['title']}")
+            print(f"subreddit: {edata['subreddit']}")
+            print(f"url: {edata['url']}")
+            print(f"permalink: {edata['permalink']}")
+            print(f"selftext_html: {edata['selftext_html']}")
+            print("*******************************************")
     else:
-        print "Something went wrong!"
+        print("Something went wrong!")
 
 
 if __name__ == "__main__":
